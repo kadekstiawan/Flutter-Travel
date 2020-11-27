3bb6c791e8295de302a497fed0a362a779bac331
kadekstiawan <kadekstiawan0@gmail.com>
2020-11-27 16:02:41 +0700

    Travel

---
+++ lib/util/places.dart
@@ -0,0 +1,62 @@
+List places = [
+  {
+    "name": "Diamond Beach",
+    "img": "assets/1.jpg",
+    "price": r"100k/Day",
+    "location": "Bali, Indonesia",
+    "details": "Pellentesque in ipsum id orci porta dapibus. "
+        "Nulla porttitor accumsan tincidunt. Donec rutrum "
+        "congue leo eget malesuada. "
+        "\n\nPraesent sapien massa, convallis a pellentesque "
+        "nec, egestas non nisi. Donec rutrum congue leo eget malesuada. "
+        "Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. "
+        "Sed porttitor lectus nibh. Donec sollicitudin molestie malesuada. "
+        "\nCurabitur arcu erat, accumsan id imperdiet et, porttitor at sem. "
+        "Vestibulum ac diam sit amet quam vehicula elementum sed sit amet dui.",
+  },
+  {
+    "name": "Klingking Beach",
+    "img": "assets/2.jpg",
+    "price": r"120k/Day",
+    "location": "Bali, Indonesia",
+    "details": "Pellentesque in ipsum id orci porta dapibus. "
+        "Nulla porttitor accumsan tincidunt. Donec rutrum "
+        "congue leo eget malesuada. "
+        "\nPraesent sapien massa, convallis a pellentesque "
+        "nec, egestas non nisi. Donec rutrum congue leo eget malesuada. "
+        "Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. "
+        "Sed porttitor lectus nibh. Donec sollicitudin molestie malesuada. "
+        "\nCurabitur arcu erat, accumsan id imperdiet et, porttitor at sem. "
+        "Vestibulum ac diam sit amet quam vehicula elementum sed sit amet dui.",
+  },
+  {
+    "name": "Pulau Padar",
+    "img": "assets/3.jpg",
+    "price": r"400k/Day",
+    "location": "NTT, Indonesia",
+    "details": "Pellentesque in ipsum id orci porta dapibus. "
+        "Nulla porttitor accumsan tincidunt. Donec rutrum "
+        "congue leo eget malesuada. "
+        "\n\nPraesent sapien massa, convallis a pellentesque "
+        "nec, egestas non nisi. Donec rutrum congue leo eget malesuada. "
+        "Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. "
+        "Sed porttitor lectus nibh. Donec sollicitudin molestie malesuada. "
+        "\nCurabitur arcu erat, accumsan id imperdiet et, porttitor at sem. "
+        "Vestibulum ac diam sit amet quam vehicula elementum sed sit amet dui.",
+  },
+  {
+    "name": "Mountain Rinjani",
+    "img": "assets/4.jpg",
+    "price": r"250k/Day",
+    "location": "NTB, Indonesia",
+    "details": "Pellentesque in ipsum id orci porta dapibus. "
+        "Nulla porttitor accumsan tincidunt. Donec rutrum "
+        "congue leo eget malesuada. "
+        "\nPraesent sapien massa, convallis a pellentesque "
+        "nec, egestas non nisi. Donec rutrum congue leo eget malesuada. "
+        "Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. "
+        "Sed porttitor lectus nibh. Donec sollicitudin molestie malesuada. "
+        "\nCurabitur arcu erat, accumsan id imperdiet et, porttitor at sem. "
+        "Vestibulum ac diam sit amet quam vehicula elementum sed sit amet dui.",
+  },
+];
